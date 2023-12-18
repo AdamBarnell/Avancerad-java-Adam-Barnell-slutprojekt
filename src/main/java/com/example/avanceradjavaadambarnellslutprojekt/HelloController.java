e8bdd1040e365ed6def2f23dc5219f36138fe043
AdamBarnell <151514125+AdamBarnell@users.noreply.github.com>
2023-12-18 15:58:41 +0100

    Update

--- src/main/java/com/example/avanceradjavaadambarnellslutprojekt/HelloController.java
+++ src/main/java/com/example/avanceradjavaadambarnellslutprojekt/HelloController.java
@@ -1,14 +1,228 @@
 package com.example.avanceradjavaadambarnellslutprojekt;
 
+import com.eclipsesource.json.Json;
+import com.eclipsesource.json.JsonObject;
+import com.eclipsesource.json.JsonArray;
+import javafx.application.Platform;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextArea;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HelloController {
-    @FXML
-    private Label welcomeText;
+
+    public Button randomRecipeButton;
+    public Button saveButton;
+    public Button getRecipe;
+    @FXML private ChoiceBox<String> recipeChoiceBox;
+    @FXML private Button deleteRecipeButton;
+    @FXML private AnchorPane ap;
+    @FXML private TextArea recipesTextArea;
+    @FXML private TextArea recipeTextArea;
+    private Map<String, String> recipeMap = new HashMap<>();
+    //Sätter olika strings för de apierna, detta gör det enklare att hämta senare
+    private static final String FIREBASE_URL = "https://slutprojekt-8495f-default-rtdb.europe-west1.firebasedatabase.app/.json";
+    private static final String API_KEY = "2da3e217a3f349d088a263038d50cef7";
+    private static final String RANDOM_RECIPE_URL = "https://api.spoonacular.com/recipes/random?apiKey=" + API_KEY;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize() {
+        loadRecipesFirebase();
+    }
+    @FXML
+    void page3(MouseEvent event) {
+        loadPage("Recipe.fxml");
+    }
+
+    @FXML
+    void page4(MouseEvent event) {
+        loadPage("Page4.fxml");
+    }
+
+    @FXML
+    void randomizer(MouseEvent event) {
+        loadPage("Randomizer.fxml");
+    }
+
+    @FXML
+    void recipes(MouseEvent event) {
+        loadPage("Page1.fxml");
+    }
+
+    private void loadPage(String page) {
+        try {
+            Node pane = FXMLLoader.load(getClass().getResource(page));
+            ap.getChildren().setAll(pane);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void saveRecipe(String recipe) {
+        HttpClient client = HttpClient.newHttpClient();
+        Map<String, String> data = new HashMap<>();
+        data.put("recipe", recipe);
+
+        try {
+            JsonObject json = new JsonObject();
+            data.forEach(json::add);
+
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(URI.create(FIREBASE_URL))
+                    .header("Content-Type", "application/json")
+                    .POST(HttpRequest.BodyPublishers.ofString(json.toString()))
+                    .build();
+
+            client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                    .thenApply(HttpResponse::body)
+                    .thenAccept(responseBody -> System.out.println("Firebase response: " + responseBody));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void saveButton(ActionEvent event) {
+        String recipeText = recipeTextArea.getText();
+        if (!recipeText.isEmpty()) {
+            saveRecipe(recipeText);
+        }
+    }
+
+    @FXML
+    private void getRandomRecipe(ActionEvent event) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(RANDOM_RECIPE_URL))
+                .build();
+
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(this::processJson)
+                .join();
+    }
+
+    private void processJson(String responseBody) {
+        try {
+            JsonArray recipesArray = Json.parse(responseBody).asObject().get("recipes").asArray();
+            if (!recipesArray.isEmpty()) {
+                JsonObject recipe = recipesArray.get(0).asObject();
+                String title = recipe.get("title").asString();
+                String instructions = recipe.get("instructions").asString();
+
+                Platform.runLater(() -> recipeTextArea.setText("Title: " + title + "\n\nInstructions:\n" + instructions));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Platform.runLater(() -> recipeTextArea.setText("Failed to load recipe."));
+        }
+    }
+
+    @FXML
+    private void getRecipe(ActionEvent event) {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(FIREBASE_URL))
+                .GET()
+                .build();
+
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(this::showRecipes)
+                .join();
+    }
+
+    private void showRecipes(String responseBody) {
+        try {
+            JsonObject rootNode = Json.parse(responseBody).asObject();
+            StringBuilder recipes = new StringBuilder();
+            for (JsonObject.Member entry : rootNode) {
+                String key = entry.getName();
+                String recipe = entry.getValue().asObject().getString("recipe", "");
+                recipes.append("\nRecipe: ").append(recipe).append("\n\n");
+            }
+
+            String finalRecipes = recipes.toString();
+            Platform.runLater(() -> recipesTextArea.setText(finalRecipes.isEmpty() ? "No recipes found." : finalRecipes));
+        } catch (Exception e) {
+            e.printStackTrace();
+            Platform.runLater(() -> recipesTextArea.setText("Failed to fetch recipes."));
+        }
+    }
+
+    @FXML
+    private void deleteRecipe(ActionEvent event) {
+        String selectedTitle = recipeChoiceBox.getValue();
+        if (selectedTitle != null && recipeMap.containsKey(selectedTitle)) {
+            String selectedKey = recipeMap.get(selectedTitle);
+            deleteRecipeFirebase(selectedKey);
+        } else {
+        }
+    }
+
+    private void deleteRecipeFirebase(String recipeKey) {
+        String deleteUrl = FIREBASE_URL.replace(".json", "/" + recipeKey + ".json");
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(deleteUrl)).DELETE().build();
+
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(responseBody -> {
+                    loadRecipesFirebase();
+                });
+    }
+
+    private void loadRecipes(String responseBody) {
+        try {
+            JsonObject recipes = Json.parse(responseBody).asObject();
+            recipeMap.clear();
+            Platform.runLater(() -> {
+                recipeChoiceBox.getItems().clear();
+                for (JsonObject.Member member : recipes) {
+                    String recipeKey = member.getName();
+                    JsonObject recipeObj = member.getValue().asObject();
+                    String fullRecipeText = recipeObj.getString("recipe", "");
+
+                    // Getting the recipetitle from the whole text
+                    String recipeTitle = titleExtraction(fullRecipeText);
+
+                    recipeMap.put(recipeTitle, recipeKey);
+                    recipeChoiceBox.getItems().add(recipeTitle);
+                }
+            });
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.out.println("Error parsing recipe data: " + e.getMessage());
+        }
+    }
+
+    private String titleExtraction(String fullRecipeText) {
+        int endIndex = fullRecipeText.indexOf("\n\nInstructions:\n");
+        if (endIndex != -1) {
+            return fullRecipeText.substring(0, endIndex);
+        } else {
+            return "Unnamed Recipe";
+        }
+    }
+    private void loadRecipesFirebase() {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(FIREBASE_URL)).GET().build();
+
+        client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
+                .thenApply(HttpResponse::body)
+                .thenAccept(this::loadRecipes)
+                .join();
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,8 @@ module com.example.avanceradjavaadambarnellslutprojekt {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.net.http;
+    requires minimal.json;
 
     opens com.example.avanceradjavaadambarnellslutprojekt to javafx.fxml;
     exports com.example.avanceradjavaadambarnellslutprojekt;
